4933bb210bb1e2bc78de33e00b84c1a35f830478
Xiaolan Lin <lin.xiaol@husky.neu.edu>
2014-10-03 18:56:27 -0400

    add blog server

--- server.js
+++ server.js
@@ -1,9 +1,71 @@
 var express = require('express');
+var mongojs = require('mongojs');
+var db = mongojs('nodejs', ["blogs"]);
 var app = express();
 
-app.get('/', function(req, res){
-	res.send('hello world, goodbye');
+
+app.get('/addBlog', function(req, res){
+	var blog = {
+		title: req.query.title,
+		author: req.query.author,
+		context: req.query.context,
+		date: req.query.date
+	};
+	db.blogs.insert(blog, function(err,doc){
+		console.log(err);
+		res.json(doc);
+	});
+
 });
+
+app.get('/getAllBlogs', function(req, res){
+	db.blogs.find(function(err, data){
+		res.json(data);
+	});
+});
+
+app.get('/deleteBlog/:id', function(req,res){
+	var id = req.params.id;
+	db.blogs.remove({
+		_id:mongojs.ObjectId(id)
+	}, function(err, doc){
+		res.json(doc);
+	});
+
+	console.log("deleteBlogbyId" + id);
+
+});
+
+app.get('/updateBlog/:id', function(req, res){
+	var blog = {
+		title: req.query.title,
+		author: req.query.author,
+		context: req.query.context,
+		date: req.query.date
+	};
+	db.blogs.findAndModify({
+		query:{_id:mongojs.ObjectId(req.params.id)},
+		update: {$set: {title:req.query.title,
+			author:req.query.author,
+			context:req.query.context,
+			date: req.query.date}},
+		new: true
+	}, function(err, doc, lastErrorObject){
+		res.json(doc);
+	});
+
+});
+
+app.get('/addBlog/:id', function(req, res){
+	var id = req.params.id;
+	db.blogs.remove(
+		{_id:mongojs.ObjectId(id)}, 
+		function(err,doc){
+			res.json(doc);
+		});
+});
+
+ //===============Connection Settings=====================//
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
